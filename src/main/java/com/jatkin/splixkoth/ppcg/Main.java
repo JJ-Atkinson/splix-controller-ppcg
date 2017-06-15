06dd13a0f71c4895721f08c57fa61fc6f1315bfe
JJ-Atkinson <jj_atkinson@att.net>
2017-06-15 09:41:48 -0500

    Updating the runner

--- src/main/java/com/jatkin/splixkoth/ppcg/Main.java
+++ src/main/java/com/jatkin/splixkoth/ppcg/Main.java
@@ -7,6 +7,7 @@ import com.jatkin.splixkoth.ppcg.players.HunterBot;
 import com.jatkin.splixkoth.ppcg.players.TrapBot;
 import com.nmerrill.kothcomm.communication.Arguments;
 import com.nmerrill.kothcomm.game.KotHComm;
+import javafx.application.Application;
 
 import java.util.Random;
 import java.util.function.Supplier;
@@ -16,31 +17,35 @@ import java.util.function.Supplier;
  */ 
 public class Main {
     public static void main(String[] args) {
-        runGames(args, 
+        SplixArguments arguments = new SplixArguments();
+        Arguments.parse(args, arguments);
+
+        Application.launch();
+        
+        runGames(arguments, 
+                args,
                 new String[]{"TrapBot 1.0", "HunterBot 1.0"},
                 new Supplier[] {TrapBot::new, HunterBot::new});
     }
-
+    
 
     /**
      * 
      * @param args
      */
-    static void runGames(String[] args, String[] botNames, Supplier<SplixPlayer>[] botConstructors) {
-        Arguments arguments = new Arguments();
-        arguments.iterations = 1000;
+    static void runGames(SplixArguments args, String[] appargs, String[] botNames, Supplier<SplixPlayer>[] botConstructors) {
+        args.iterations = 1000;
         long now = System.currentTimeMillis();
         KotHCommMultiThread<SplixPlayer, SplixGame> runner = new KotHCommMultiThread<>(() -> new SplixGame(200));
         runner.setThreadCount(8);
-//        runner.
 
         for (int i = 0; i < botNames.length; i++) {
             runner.addSubmission(botNames[i], botConstructors[i]);
         }
         
         runner.setGameSize(2);
-        runner.setArgumentParser(arguments);
-        runner.run(args);
+        runner.setArgumentParser(args);
+        runner.run(appargs);
         System.out.println("Time to run: " + (System.currentTimeMillis() - now));
     }
 }
---
+++ src/main/java/com/jatkin/splixkoth/ppcg/SplixArguments.java
@@ -0,0 +1,23 @@
+package com.jatkin.splixkoth.ppcg;
+
+import com.beust.jcommander.Parameter;
+import com.nmerrill.kothcomm.communication.Arguments;
+
+/**
+ * Created by Jarrett on 06/15/17.
+ */
+public class SplixArguments extends Arguments {
+    @Parameter(
+        names = {"-m", "--multi-thread"},
+        description = "Allow multi-threaded running (same as `--thread-count 1`)"
+    )
+    public boolean multiThread = true;
+    
+    @Parameter(
+        names = {"-c", "--thread-count"},
+        description = "Number of threads used to process the game"
+    )
+    public int threadCount = 4;
+    
+    
+}
